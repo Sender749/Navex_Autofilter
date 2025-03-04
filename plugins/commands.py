a16d3604f28b737f3dd5108abfa2b567d7679925
tgbot69 <kd142047@gmail.com>
2025-03-04 22:03:38 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -194,7 +194,7 @@ async def start(client:Client, message):
                             InlineKeyboardButton('Tᴏᴘ Tʀᴇɴᴅɪɴɢ ⚡', callback_data="trending")
                         ]] 
                         reply_markup = InlineKeyboardMarkup(buttons)
-                        m=await message.reply_sticker("https://envs.sh/Pjn.webm") 
+                        m=await message.reply_sticker("CAACAgQAAxkBAAEN9q9nxyqs1yDrRQ_R2ZyoW1wVG4jw8wACRRcAAmArCVFO4FTIzy69ajYE") 
                         await asyncio.sleep(1)
                         await m.delete()
                         await message.reply_photo(photo=random.choice(START_IMG), caption=script.START_TXT.format(message.from_user.mention, get_status(), message.from_user.id),
@@ -223,7 +223,7 @@ async def start(client:Client, message):
                             InlineKeyboardButton('Tᴏᴘ Tʀᴇɴᴅɪɴɢ ⚡', callback_data="trending")
                         ]] 
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("https://envs.sh/Pjn.webm") 
+        m=await message.reply_sticker("CAACAgQAAxkBAAEN9q9nxyqs1yDrRQ_R2ZyoW1wVG4jw8wACRRcAAmArCVFO4FTIzy69ajYE") 
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(photo=random.choice(START_IMG), caption=script.START_TXT.format(message.from_user.mention, get_status(), message.from_user.id),
@@ -1014,7 +1014,7 @@ async def refer(bot, message):
         InlineKeyboardButton(f'⏳ {referdb.get_refer_points(message.from_user.id)}', callback_data='ref_point'),
         InlineKeyboardButton('Close', callback_data='close_data')
     ]]  
-    m=await message.reply_sticker("https://envs.sh/PR4.tgs")      
+    m=await message.reply_sticker("CAACAgQAAxkBAAEN9q1nxyong4UZaPYBjx18grmb6ig2awACpRcAAqFyCVEIhIMkevWwdzYE")      
     await m.delete()
     reply_markup = InlineKeyboardMarkup(btn)
     await message.reply_photo(
