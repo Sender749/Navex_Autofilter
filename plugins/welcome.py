d81f6f2ffd8b01180cfc9775959cf82ce13524e5
Sender749 <jotmahal2003@gmail.com>
2025-03-08 18:35:36 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -1,14 +1,15 @@
 from pyrogram import Client, filters
-from Script import script  # Import script.py where messages are stored
+from pyrogram.types import ChatMemberUpdated
+from Script import script  # Import script.py where WELCOME_TXT is stored
 
-@Client.on_message(filters.new_chat_members & filters.group)
-async def welcome_new_user(client, message):
-    for new_member in message.new_chat_members:
-        user_mention = new_member.mention  # Get new user's mention
-        group_name = message.chat.title  # Get group name
+@Client.on_chat_member_updated(filters.group)
+async def welcome_user(client, chat_member: ChatMemberUpdated):
+    if chat_member.new_chat_member and chat_member.new_chat_member.status in ["member"]:
+        user_mention = chat_member.new_chat_member.user.mention  # Get user's mention
+        group_name = chat_member.chat.title  # Get group name
 
         # Get welcome message from script.py
         welcome_text = script.WELCOME_TXT.format(user_mention=user_mention, group_name=group_name)
 
         # Send the welcome message
-        await message.reply_text(welcome_text)
+        await client.send_message(chat_member.chat.id, welcome_text)
