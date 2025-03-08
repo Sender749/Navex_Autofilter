3047a38f243a94351d096cb659b2be5b32335e95
Sender749 <jotmahal2003@gmail.com>
2025-03-08 13:05:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/Navexdisscussion') #Sup
 #---------------------------------------------------------------
 IS_VERIFY = is_enabled('IS_VERIFY', True)
 #---------------------------------------------------------------
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/Navexdisscussion/4")
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/Navexdisscussion/33")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/Navexdisscussion/4")
 TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/Navexdisscussion/4")
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
