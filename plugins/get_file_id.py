05863c937b941f9fd5c0eebaeaf40ea69ea54dad
Sender749 <jotmahal2003@gmail.com>
2025-03-08 21:06:00 +0530

    Update get_file_id.py

--- plugins/get_file_id.py
+++ plugins/get_file_id.py
@@ -1,7 +1,27 @@
 from pyrogram import Client, filters
 
-@Client.on_message(filters.video)
-async def get_video_file_id(client, message):
-    video = message.video
-    file_id = video.file_id
-    await message.reply_text(f"🎥 **Video File ID:**\n`{file_id}`")
+@Client.on_message(filters.media)
+async def get_media_file_id(client, message):
+    # Check the type of media and extract the file ID accordingly
+    file_id = None
+
+    if message.video:
+        file_id = message.video.file_id
+        media_type = "Video"
+    elif message.document:
+        file_id = message.document.file_id
+        media_type = "File"
+    elif message.animation:
+        file_id = message.animation.file_id
+        media_type = "GIF"
+    elif message.sticker:
+        file_id = message.sticker.file_id
+        media_type = "Sticker"
+    else:
+        media_type = "Unknown"
+
+    if file_id:
+        # Send the file ID back to the user
+        await message.reply_text(f"🎬 **{media_type} File ID:**\n`{file_id}`")
+    else:
+        await message.reply_text("This media type doesn't have a file ID.")
