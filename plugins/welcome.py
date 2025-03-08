e7fdc3bab72f0f77f16e5c83d83492ba51bd5080
Sender749 <jotmahal2003@gmail.com>
2025-03-08 19:33:02 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -1,39 +1,21 @@
-from pyrogram import Client, filters
-from Script import script  
-from info import BIN_CHANNEL  # Import bin channel ID
+# plugins/welcome.py
 
-@Client.on_message(filters.group & filters.new_chat_members)
-async def welcome(client, message):
-    for new_member in message.new_chat_members:
-        user_mention = new_member.mention  
-        user_id = new_member.id  
-        username = f"@{new_member.username}" if new_member.username else "No Username"
-        group_name = message.chat.title  
-        inviter = message.from_user  # Who added the user (if available)
+from telegram import Update
+from telegram.ext import CallbackContext
+from script import WELCOME_MESSAGE, WELCOME_VIDEO_PATH
 
-        # Determine how the user joined
-        if inviter:
-            if inviter.is_bot:
-                join_method = "Joined via Bot Message"
-            else:
-                join_method = f"Invited by {inviter.mention}"
-        elif message.sender_chat:
-            join_method = "Joined via Group Link"
-        else:
-            join_method = "Joined via Search"
+async def send_welcome_message(update: Update, context: CallbackContext):
+    # Get the new user's details
+    user = update.message.new_chat_members[0]
+    user_name = user.first_name
 
-        # Welcome message in group
-        welcome_text = script.WELCOME_TXT.format(user_mention=user_mention, group_name=group_name)
-        await message.reply_text(welcome_text)
+    # Format the welcome message with the user's name
+    welcome_text = WELCOME_MESSAGE.format(user_name=user_name)
 
-        # Log message to bin channel
-        bin_message = (
-            f"🆕 **New User Joined!**\n\n"
-            f"👤 **Name:** {user_mention}\n"
-            f"🆔 **User ID:** `{user_id}`\n"
-            f"🔗 **Username:** {username}\n"
-            f"🏠 **Group:** {group_name}\n"
-            f"🛠 **Join Method:** {join_method}"
-        )
+    # Send the welcome message
+    await update.message.reply_text(welcome_text)
 
-        await client.send_message(BIN_CHANNEL, bin_message)
+    # Send the playable video
+    if WELCOME_VIDEO_PATH:
+        with open(WELCOME_VIDEO_PATH, 'rb') as video_file:
+            await update.message.reply_video(video=video_file, caption="Check out this intro video!")
