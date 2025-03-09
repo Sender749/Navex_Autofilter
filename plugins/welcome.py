27b7cfd5e10e6b78f80c430e4db7fdbd45e36d80
Sender749 <jotmahal2003@gmail.com>
2025-03-09 16:47:49 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -1,23 +1,23 @@
-from pyrogram import Client, filters
-from info import BIN_CHANNEL  # Importing bin channel ID from info.py
+from pyrogram import filters
+from info import BIN_CHANNEL_ID  # Import bin channel ID
+from Jisshu.bot import JisshuBot  # Import bot instance
 
 # Tutorial video file_id
 TUTORIAL_VIDEO_FILE_ID = "BAACAgUAAxkBAAIN3GfMYg3f98IOATHjOljUWUSK8D_MAAJ1FQACyBhRVjH6nRmO9C86HgQ"
 
-# Flags to enable or disable messages
+# Flags to control message sending
 SEND_WELCOME_MESSAGE = True  # Set False to disable welcome message
 SEND_TUTORIAL_VIDEO = True   # Set False to disable tutorial video
-SEND_JOIN_LOG = True  # Set False to disable logging join messages in bin channel
+SEND_JOIN_LOG = True  # Set False to disable logging join messages
 
-@Client.on_message(filters.new_chat_members)
+@JisshuBot.on_message(filters.new_chat_members)
 async def welcome_new_user(client, message):
-    chat_id = message.chat.id  # Group chat ID
+    chat_id = message.chat.id  # Group ID
 
-    # Iterate through new members
     for user in message.new_chat_members:
         user_name = user.first_name
 
-        # Welcome message
+        # Welcome text
         welcome_text = f"👋 **Welcome {user_name}!**\n\n" \
                        "🔍 You can search any movies in this group by sending their name (with correct spelling). " \
                        "Just type the movie name and check the results!\n\n" \
@@ -34,5 +34,4 @@ async def welcome_new_user(client, message):
 
         # Send join log to bin channel if enabled
         if SEND_JOIN_LOG:
-            await client.send_message(BIN_CHANNEL, f"📝 **User joined:** {user_name}")
-
+            await client.send_message(BIN_CHANNEL_ID, f"📝 **User joined:** {user_name}")
