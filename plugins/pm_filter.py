cabb9e23c7dde6d2c5b1b8f6e968213bacf65ac5
Sender749 <jotmahal2003@gmail.com>
2025-03-18 19:40:54 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -182,15 +182,9 @@ async def next_page(bot, query):
             display_text = file.caption if file.caption else file.file_name
             links += f"""<b>\n\n{file_num}. <a href=https://t.me/{temp.U_NAME}?start=file_{query.message.chat.id}_{file.file_id}>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), display_text.split()))}</a></b>"""
     else:
-        btn = [
-            [
-                InlineKeyboardButton(
-                    text=f"🔗 {get_size(file.file_size)}≽ {formate_file_name(file.caption if file.caption else file.file_name)}",
-                    callback_data=f'cfiles#{reqnxt}#{file.file_id}'
-                )
-            ]
-            for file in files
-        ]
+        btn = [[InlineKeyboardButton(text=f"📁 {get_size(file.file_size)}≽ {formate_file_name(file.caption if file.caption else file.file_name)}", url=f'https://telegram.dog/{temp.U_NAME}?start=file_{query.message.chat.id}_{file.file_id}'),]
+                for file in files
+              ]
 
     btn.insert(0, [
         InlineKeyboardButton("📥 𝗦𝗲𝗻𝗱 𝗔𝗹𝗹 𝗙𝗶𝗹𝗲𝘀 📥", callback_data=batch_link),
@@ -321,15 +315,9 @@ async def season_search(client: Client, query: CallbackQuery):
             display_text = file.caption if file.caption else file.file_name
             links += f"""<b>\n\n{file_num}. <a href=https://t.me/{temp.U_NAME}?start=file_{query.message.chat.id}_{file.file_id}>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), display_text.split()))}</a></b>"""
     else:
-        btn = [
-            [
-                InlineKeyboardButton(
-                    text=f"🔗 {get_size(file.file_size)}≽ {formate_file_name(file.caption if file.caption else file.file_name)}",
-                    callback_data=f'cfiles#{reqnxt}#{file.file_id}'
-                )
-            ]
-            for file in files
-        ]
+        btn = [[InlineKeyboardButton(text=f"📁 {get_size(file.file_size)}≽ {formate_file_name(file.caption if file.caption else file.file_name)}", url=f'https://telegram.dog/{temp.U_NAME}?start=file_{query.message.chat.id}_{file.file_id}'),]
+                for file in files
+              ]
    
     btn.insert(0,[
 	InlineKeyboardButton("📥 𝗦𝗲𝗻𝗱 𝗔𝗹𝗹 𝗙𝗶𝗹𝗲𝘀 📥", callback_data=batch_link),
@@ -433,17 +421,10 @@ async def year_search(client: Client, query: CallbackQuery):
             display_text = file.caption if file.caption else file.file_name
             links += f"""<b>\n\n{file_num}. <a href=https://t.me/{temp.U_NAME}?start=file_{query.message.chat.id}_{file.file_id}>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), display_text.split()))}</a></b>"""
     else:
-        btn = [
-            [
-                InlineKeyboardButton(
-                    text=f"🔗 {get_size(file.file_size)}≽ {formate_file_name(file.caption if file.caption else file.file_name)}",
-                    callback_data=f'cfiles#{reqnxt}#{file.file_id}'
-                )
-            ]
-            for file in files
-        ]
+        btn = [[InlineKeyboardButton(text=f"📁 {get_size(file.file_size)}≽ {formate_file_name(file.caption if file.caption else file.file_name)}", url=f'https://telegram.dog/{temp.U_NAME}?start=file_{query.message.chat.id}_{file.file_id}'),]
+                for file in files
+              ]
         
-   
     btn.insert(0,[
 	InlineKeyboardButton("📥 𝗦𝗲𝗻𝗱 𝗔𝗹𝗹 𝗙𝗶𝗹𝗲𝘀 📥", callback_data=batch_link),
         ])
@@ -545,17 +526,10 @@ async def quality_search(client: Client, query: CallbackQuery):
             display_text = file.caption if file.caption else file.file_name
             links += f"""<b>\n\n{file_num}. <a href=https://t.me/{temp.U_NAME}?start=file_{query.message.chat.id}_{file.file_id}>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), display_text.split()))}</a></b>"""
     else:
-        btn = [
-            [
-                InlineKeyboardButton(
-                    text=f"🔗 {get_size(file.file_size)}≽ {formate_file_name(file.caption if file.caption else file.file_name)}",
-                    callback_data=f'cfiles#{reqnxt}#{file.file_id}'
-                )
-            ]
-            for file in files
-        ]
+        btn = [[InlineKeyboardButton(text=f"📁 {get_size(file.file_size)}≽ {formate_file_name(file.caption if file.caption else file.file_name)}", url=f'https://telegram.dog/{temp.U_NAME}?start=file_{query.message.chat.id}_{file.file_id}'),]
+                for file in files
+              ]
         
- 
     btn.insert(0,[
 	InlineKeyboardButton("📥 𝗦𝗲𝗻𝗱 𝗔𝗹𝗹 𝗙𝗶𝗹𝗲𝘀 📥", callback_data=batch_link),
         ])
@@ -662,17 +636,10 @@ async def lang_search(client: Client, query: CallbackQuery):
             display_text = file.caption if file.caption else file.file_name
             links += f"""<b>\n\n{file_num}. <a href=https://t.me/{temp.U_NAME}?start=file_{query.message.chat.id}_{file.file_id}>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), display_text.split()))}</a></b>"""
     else:
-        btn = [
-            [
-                InlineKeyboardButton(
-                    text=f"🔗 {get_size(file.file_size)}≽ {formate_file_name(file.caption if file.caption else file.file_name)}",
-                    callback_data=f'cfiles#{reqnxt}#{file.file_id}'
-                )
-            ]
-            for file in files
-        ]
+        btn = [[InlineKeyboardButton(text=f"📁 {get_size(file.file_size)}≽ {formate_file_name(file.caption if file.caption else file.file_name)}", url=f'https://telegram.dog/{temp.U_NAME}?start=file_{query.message.chat.id}_{file.file_id}'),]
+                for file in files
+              ]
         
-
     btn.insert(0,[
 	InlineKeyboardButton("📥 𝗦𝗲𝗻𝗱 𝗔𝗹𝗹 𝗙𝗶𝗹𝗲𝘀 📥", callback_data=batch_link),
         ])
